dc305e2a300003815057e60f57a156c72edfc2ec
CodeGod <CodeGod@outlook.com>
2023-03-31 16:47:20 +0800

    hello-git first commit

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: HelloGit
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author 钟学峰
+ * @Create 2023/3/31 16:37
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
