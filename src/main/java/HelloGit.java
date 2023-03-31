0d340af64208c4a0257619adfd4afd548bcac80e
CodeGod <CodeGod@outlook.com>
2023-03-31 18:08:16 +0800

    hello-git third commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("first update");
+        System.out.println("second update");
     }
 }
